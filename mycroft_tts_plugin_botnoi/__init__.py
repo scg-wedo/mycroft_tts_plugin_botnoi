462f135fb4ba9763bb02efa4b66f71a0ce13cc5e
natchanonSCG <natchpor@scg.com>
2021-07-09 15:52:43 +0700

    change mp3 to wav

--- mycroft_tts_plugin_botnoi/__init__.py
+++ mycroft_tts_plugin_botnoi/__init__.py
@@ -4,8 +4,7 @@ from mycroft.tts import TTS, TTSValidator
 class botnoiTTSPlugin(TTS):
     """Interface to Botnoi TTS."""
     def __init__(self, lang, config):
-        super(botnoiTTSPlugin, self).__init__(lang, config, BotnoiTTSValidator(
-            self), 'mp3')
+        super(botnoiTTSPlugin, self).__init__(lang, config, BotnoiTTSValidator(self))
 
     def get_tts(self, sentence, wav_file):
         url = "https://openapi.botnoi.ai/service-api/text2speech-female?text=สวัสดี&speaker=tonkhaow"
@@ -13,12 +12,9 @@ class botnoiTTSPlugin(TTS):
             'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MjYwNjMyMzAsImlkIjoiZjgxZDFlYmEtNTQ4Zi00ZjdiLWI1N2YtZDYxZjFhNGFlNmI3IiwiaXNzIjoiRVd0MGxYWHNzUVMzUklsMkVUVWczQnRYVEdHVkdFSlciLCJuYW1lIjoiS2VuIiwicGljIjoiaHR0cHM6Ly9wcm9maWxlLmxpbmUtc2Nkbi5uZXQvMGhMd0dtOElpUUUxaDZGRHdadUdCc0QwWlJIVFVOT2hVUUFub0lPRmhIU1d4ZWNWWUdUM05VYXcxSEhUOVFkd0VQUlNkWk9Bb1hTejFRIn0.qcaqEGfARKFyGRCLnMvPDsCnLgwNaWvnaHh6wDPzmPo'
         }
         response = requests.request("GET", url, headers = headers)
-        # with open(str(wav_file), 'wb') as file:
-        #     file.write(response.content)
-        # return (wav_file, None)  # No phonemes
-        with open(str('tts.mp3'), 'wb') as f:
-            f.write(response.content)
-        return ('tts.mp3', None)
+        with open(str(wav_file), 'wb') as file:
+            file.write(response.content)
+        return (wav_file, None)  # No phonemes
 
 class BotnoiTTSValidator(TTSValidator):
     def __init__(self, tts):
