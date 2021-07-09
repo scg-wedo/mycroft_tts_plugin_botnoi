2a7e98c31868c46afc676bc6d0a241be8d70e12a
natchanonSCG <natchpor@scg.com>
2021-07-09 15:43:19 +0700

    try hardcoded on get_tts

--- mycroft_tts_plugin_botnoi/__init__.py
+++ mycroft_tts_plugin_botnoi/__init__.py
@@ -13,9 +13,12 @@ class botnoiTTSPlugin(TTS):
             'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MjYwNjMyMzAsImlkIjoiZjgxZDFlYmEtNTQ4Zi00ZjdiLWI1N2YtZDYxZjFhNGFlNmI3IiwiaXNzIjoiRVd0MGxYWHNzUVMzUklsMkVUVWczQnRYVEdHVkdFSlciLCJuYW1lIjoiS2VuIiwicGljIjoiaHR0cHM6Ly9wcm9maWxlLmxpbmUtc2Nkbi5uZXQvMGhMd0dtOElpUUUxaDZGRHdadUdCc0QwWlJIVFVOT2hVUUFub0lPRmhIU1d4ZWNWWUdUM05VYXcxSEhUOVFkd0VQUlNkWk9Bb1hTejFRIn0.qcaqEGfARKFyGRCLnMvPDsCnLgwNaWvnaHh6wDPzmPo'
         }
         response = requests.request("GET", url, headers = headers)
-        with open(str(wav_file), 'wb') as file:
-            file.write(response.content)
-        return (wav_file, None)  # No phonemes
+        # with open(str(wav_file), 'wb') as file:
+        #     file.write(response.content)
+        # return (wav_file, None)  # No phonemes
+        with open(str('tts.mp3'), 'wb') as f:
+            f.write(response.content)
+        return ('tts.mp3', None)
 
 class BotnoiTTSValidator(TTSValidator):
     def __init__(self, tts):
