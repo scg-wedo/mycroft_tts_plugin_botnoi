f6af609b6eaa82aea7d0159c705b9bd70bf886ce
natchanonSCG <natchpor@scg.com>
2021-07-09 16:37:52 +0700

    update BotnoiTTSValidator

--- mycroft_tts_plugin_botnoi/__init__.py
+++ mycroft_tts_plugin_botnoi/__init__.py
@@ -4,8 +4,7 @@ from mycroft.tts import TTS, TTSValidator
 class botnoiTTSPlugin(TTS):
     """Interface to Botnoi TTS."""
     def __init__(self, lang, config):
-        super().__init__(lang, config, BotnoiTTSValidator(self), audio_ext='wav',
-                         phonetic_spelling=False, ssml_tags=None)
+        super().__init__(lang, config, BotnoiTTSValidator(self))
 
     def get_tts(self, sentence, wav_file):
         url = "https://openapi.botnoi.ai/service-api/text2speech-female?text=สวัสดี&speaker=tonkhaow"
@@ -22,22 +21,10 @@ class BotnoiTTSValidator(TTSValidator):
         super(BotnoiTTSValidator, self).__init__(tts)
 
     def validate_lang(self):
-        lang = self.tts.lang
-        if lang.lower() != "th":
-            raise ValueError("Language not supported by botnoiTTS: {}"
-                             .format(lang))
+        pass
 
     def validate_connection(self):
-        try:
-            url = "https://openapi.botnoi.ai/service-api/text2speech-female?text=สวัสดี&speaker=tonkhaow"
-            headers = {
-                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MjYwNjMyMzAsImlkIjoiZjgxZDFlYmEtNTQ4Zi00ZjdiLWI1N2YtZDYxZjFhNGFlNmI3IiwiaXNzIjoiRVd0MGxYWHNzUVMzUklsMkVUVWczQnRYVEdHVkdFSlciLCJuYW1lIjoiS2VuIiwicGljIjoiaHR0cHM6Ly9wcm9maWxlLmxpbmUtc2Nkbi5uZXQvMGhMd0dtOElpUUUxaDZGRHdadUdCc0QwWlJIVFVOT2hVUUFub0lPRmhIU1d4ZWNWWUdUM05VYXcxSEhUOVFkd0VQUlNkWk9Bb1hTejFRIn0.qcaqEGfARKFyGRCLnMvPDsCnLgwNaWvnaHh6wDPzmPo'
-            }
-            requests.request("GET", url, headers = headers)
-        except Exception:
-            raise Exception(
-                'BotnoiTTS server could not be verified. Please check your '
-                'internet connection.')
+        pass
 
     def get_tts_class(self):
-        return BotnoiTTSValidator
+        return botnoiTTSPlugin
