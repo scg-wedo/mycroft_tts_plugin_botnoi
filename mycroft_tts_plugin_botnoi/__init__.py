596d4e13b2aad742e5c28a8f32143aa078eb65d4
natchanonSCG <natchpor@scg.com>
2021-07-14 08:21:08 +0700

    add token

--- mycroft_tts_plugin_botnoi/__init__.py
+++ mycroft_tts_plugin_botnoi/__init__.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*- 
 
+from typing import TextIO
 import requests
 from mycroft.tts import TTS, TTSValidator
+token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MjY0ODQ3ODcsImlkIjoiZjgxZDFlYmEtNTQ4Zi00ZjdiLWI1N2YtZDYxZjFhNGFlNmI3IiwiaXNzIjoiRVd0MGxYWHNzUVMzUklsMkVUVWczQnRYVEdHVkdFSlciLCJuYW1lIjoiS2VuIiwicGljIjoiaHR0cHM6Ly9wcm9maWxlLmxpbmUtc2Nkbi5uZXQvMGhMd0dtOElpUUUxaDZGRHdadUdCc0QwWlJIVFVOT2hVUUFub0lPRmhIU1d4ZWNWWUdUM05VYXcxSEhUOVFkd0VQUlNkWk9Bb1hTejFRIn0.MiNZ_lnuIUpzZ6slTqTzgM41XqiJvxWdgbuWYS-wfbU'
 
 class botnoiTTSPlugin(TTS):
     """Interface to Botnoi TTS."""
@@ -12,7 +14,7 @@ class botnoiTTSPlugin(TTS):
     def get_tts(self, sentence, wav_file):
         url = "https://openapi.botnoi.ai/service-api/text2speech-female?text="+str(sentence)+"&speaker=tonkhaow"
         headers = {
-            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MjYzMDgxMjYsImlkIjoiZjgxZDFlYmEtNTQ4Zi00ZjdiLWI1N2YtZDYxZjFhNGFlNmI3IiwiaXNzIjoiRVd0MGxYWHNzUVMzUklsMkVUVWczQnRYVEdHVkdFSlciLCJuYW1lIjoiS2VuIiwicGljIjoiaHR0cHM6Ly9wcm9maWxlLmxpbmUtc2Nkbi5uZXQvMGhMd0dtOElpUUUxaDZGRHdadUdCc0QwWlJIVFVOT2hVUUFub0lPRmhIU1d4ZWNWWUdUM05VYXcxSEhUOVFkd0VQUlNkWk9Bb1hTejFRIn0.jFbxULw3Mm9zY47n-QGVVQSsYUhy_T1Xqh_zoBrnHCY'
+            'Authorization': 'Bearer '+str(token)
         }
         response = requests.request("GET", url, headers = headers)
         with open(wav_file, 'wb') as file:
