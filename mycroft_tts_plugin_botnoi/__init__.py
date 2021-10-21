3bb04b6cdacb683b9ff43f9e825e21b146c19292
natchanonSCG <natchpor@scg.com>
2021-10-21 17:02:54 +0700

    remove botnoi token

--- mycroft_tts_plugin_botnoi/__init__.py
+++ mycroft_tts_plugin_botnoi/__init__.py
@@ -4,7 +4,6 @@
 from typing import TextIO
 import requests
 from mycroft.tts import TTS, TTSValidator
-token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MjY0ODQ3ODcsImlkIjoiZjgxZDFlYmEtNTQ4Zi00ZjdiLWI1N2YtZDYxZjFhNGFlNmI3IiwiaXNzIjoiRVd0MGxYWHNzUVMzUklsMkVUVWczQnRYVEdHVkdFSlciLCJuYW1lIjoiS2VuIiwicGljIjoiaHR0cHM6Ly9wcm9maWxlLmxpbmUtc2Nkbi5uZXQvMGhMd0dtOElpUUUxaDZGRHdadUdCc0QwWlJIVFVOT2hVUUFub0lPRmhIU1d4ZWNWWUdUM05VYXcxSEhUOVFkd0VQUlNkWk9Bb1hTejFRIn0.MiNZ_lnuIUpzZ6slTqTzgM41XqiJvxWdgbuWYS-wfbU'
 
 class botnoiTTSPlugin(TTS):
     """Interface to Botnoi TTS."""
@@ -12,11 +11,8 @@ class botnoiTTSPlugin(TTS):
         super(botnoiTTSPlugin, self).__init__(lang, config, BotnoiTTSValidator(self))
 
     def get_tts(self, sentence, wav_file):
-        url = "https://openapi.botnoi.ai/service-api/text2speech-female?text="+str(sentence)+"&speaker=tonkhaow"
-        headers = {
-            'Authorization': 'Bearer '+str(token)
-        }
-        response = requests.request("GET", url, headers = headers)
+        url = "https://tts.botnoi.ai/demo/api/doctts?text="+str(sentence)+"&speaker=tonkhaow&api_key=1qL0ISiB1cjXjQ0HD-1sjjTHesZrkx9A_ym-ifUxCTWI"
+        response = requests.request("GET", url)
         with open(wav_file, 'wb') as file:
             file.write(response.content)
         return (wav_file, None)  # No phonemes
